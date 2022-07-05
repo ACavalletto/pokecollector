6fa445763ba093c9451afea3ffb4a5cdab30bdfb
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-05 13:49:26 -0600

    Created model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.IntegerField()
+    generation = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    evolution_stage = models.IntegerField()
